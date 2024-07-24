612657f5626c48d9f110a1c82b8507c100df6292
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-07-24 16:35:06 +0200

    delete all to start from scratch

--- composables/useMousePos.ts
+++
@@ -1,8 +0,0 @@
-export const useMousePos = () => {
-    const mousePos = useState('mousePos', () => ref({x: 0, y: 0}));
-
-
-    return {
-        mousePos
-    }
-}
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <new-three />
-  <detail-view />
-</template>
-
-<script setup lang="ts"></script>
