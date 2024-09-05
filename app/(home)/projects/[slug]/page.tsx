8ec9c6507fe5eaad82c31a3dac15d930da2d3d11
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2024-09-05 17:46:55 +0200

    fix: forgot to remove avatar in page.tsx

--- app/(home)/projects/[slug]/page.tsx
+++ app/(home)/projects/[slug]/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-import Avatar from "../../avatar";
 import CoverImage from "../../cover-image";
 import PortableText from "../../portable-text";
 
