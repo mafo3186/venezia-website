f15310599f7d17c620572b1607b742e9e508de75
Luca Selinski <lucaselinski@gmail.com>
2024-09-09 15:58:42 +0200

    Idea luca 3d (#28)
    * feat: add basic 3d functionality and home page
    
    * feat: improve 3d controls, implement functionality for seamless transitions to sub-pages

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,5 @@
-import Link from "next/link";
-import { groq } from "next-sanity";
-
 import PortableText from "./portable-text";
-
-import type { ProjectsQueryResult, SettingsQueryResult } from "@/sanity.types";
 import * as demo from "@/sanity/lib/demo";
-import { sanityFetch } from "@/sanity/lib/fetch";
-import { settingsQuery } from "@/sanity/lib/queries";
 
 function Intro(props: { title: string | null | undefined; description: any }) {
   const title = props.title || demo.title;
@@ -27,38 +20,6 @@ function Intro(props: { title: string | null | undefined; description: any }) {
   );
 }
 
-const projectsQuery = groq`*[_type == "project" && defined(slug.current)] | order(date desc, _updatedAt desc) {
-  content,
-  _id,
-  "status": select(_originalId in path("drafts.**") => "draft", "published"),
-  "title": coalesce(title, "Untitled"),
-  "slug": slug.current,
-  excerpt,
-  author,
-}`;
-
 export default async function Page() {
-  const [settings, projects] = await Promise.all([
-    sanityFetch<SettingsQueryResult>({
-      query: settingsQuery,
-    }),
-    sanityFetch<ProjectsQueryResult>({ query: projectsQuery }),
-  ]);
-
-  return (
-    <>
-      <Intro title={settings?.title} description={settings?.description} />
-      <ul>
-        {projects?.map((project) => (
-          <li key={project._id}>
-            <h3>
-              <Link href={`/projects/${project.slug}`} >
-                {project.title}
-              </Link>
-            </h3>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <></>;
 }
