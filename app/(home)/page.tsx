7ca2aea396beb0ec20d9a34623e563e9030240f2
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-09-10 23:56:04 +0200

    #29 remove unused code from meant to be startpage.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,7 @@
-import PortableText from "./portable-text";
-import * as demo from "@/sanity/lib/demo";
-
 function Intro(props: { title: string | null | undefined; description: any }) {
-    const title = props.title || demo.title;
-    const description = props.description?.length
-        ? props.description
-        : demo.description;
     return (
-        <section>
-            <h1>
-                {title || demo.title}
-            </h1>
-            <h2>
-                <PortableText
-                    value={description?.length ? description : demo.description}
-                />
-            </h2>
-        </section>
+        <>
+        </>
     );
 }
 
