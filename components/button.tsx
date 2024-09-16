55428b1f99323fbf2495af26768302dcc76f2731
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-09-17 01:41:42 +0200

    #16 add component Home and Back Buttons

---
+++ components/button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from 'next/link';
+import styles from './button.module.css';
+import React from "react"; 
+
+export function HomeButton () {
+  return (
+    <Link href="/" className={styles.button}>
+      Home
+    </Link>
+  );
+};
+
+
+export function BackButton() {
+  const handleBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <button onClick={handleBack} className={styles.button}>
+      Zurück
+    </button>
+  );
+};
+
