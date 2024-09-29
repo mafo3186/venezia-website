36d0628ab6ad710a701a9201b46126261599f225
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-09-30 00:20:53 +0200

    #76 figcaption fix (not calculated in showCasePiece-Size height yet, so disappears sometimes)

--- components/projectPage/showcasePiece.tsx
+++ components/projectPage/showcasePiece.tsx
@@ -21,32 +21,33 @@ export default function ShowcasePiece(props: ShowcasePieceProps) {
 
   return (
     <figure className={styles.figure}>
-      {showcase.type === "image" && (
-        <Image
-          fill
-          blurDataURL={blurhashToBase64(showcase.blurHash!)}
-          placeholder="blur"
-          src={showcase.content}
-          alt={showcase.description ?? ""}
-          className={styles.image}
-        />
-      )}
-      {showcase.type === "video" && (
-        <video controls autoPlay muted loop className={styles.video}>
-          <source src={showcase.content} type={showcase.mimeType!} />
-        </video>
-      )}
-      {showcase.type === "audio" && (
-        <div className={styles.audio}>
-          <audio controls>
-            <source src={showcase.content} type={showcase.mimeType!} />
-          </audio>
-        </div>
-      )}
-      {showcase.type === "text" && (
-        <pre className={styles.text}>{showcase.content}</pre>
-      )}
-      {showcase.type === "website" && (
+      <div className={styles.contentContainer}>
+        {showcase.type === "image" && (
+          <Image
+            fill
+            blurDataURL={blurhashToBase64(showcase.blurHash!)}
+            placeholder="blur"
+            src={showcase.content}
+            alt={showcase.description ?? ""}
+            className={styles.image}
+          />
+        )}
+        {showcase.type === "video" && (
+          <video controls autoPlay muted loop className={styles.video}>
+            <source src={showcase.content} type={showcase.mimeType!}/>
+          </video>
+        )}
+        {showcase.type === "audio" && (
+          <div className={styles.audio}>
+            <audio controls>
+              <source src={showcase.content} type={showcase.mimeType!}/>
+            </audio>
+          </div>
+        )}
+        {showcase.type === "text" && (
+          <pre className={styles.text}>{showcase.content}</pre>
+        )}
+        {showcase.type === "website" && (
           <div className={styles.websiteContainer}>
             <iframe
               className={styles.iFrame}
@@ -54,16 +55,17 @@ export default function ShowcasePiece(props: ShowcasePieceProps) {
               title={showcase.description ?? "Website"}
               scrolling="yes"
             />
-          <button 
-            className={styles.expandButton}
-            onClick={() => window.open(showcase.content, "_blank", "noopener noreferrer" )}
-            aria-label={"Öffne Webseite auf neuer Seite"}
-          >
-            <FaExpandArrowsAlt />
-          </button>
+            <button
+              className={styles.expandButton}
+              onClick={() => window.open(showcase.content, "_blank", "noopener noreferrer")}
+              aria-label={"Öffne Webseite auf neuer Seite"}
+            >
+              <FaExpandArrowsAlt/>
+            </button>
           </div>
-      )}
-      {showcase.description && <figcaption className={styles.description}>{showcase.description}</figcaption>}
+        )}
+      </div>
+        {showcase.description && <figcaption className={styles.description}>{showcase.description}</figcaption>}
     </figure>
-  );
+);
 }
