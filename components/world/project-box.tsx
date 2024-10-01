054db900dacb020857974dd502f23f57413c2644
Luca Selinski <lucaselinski@gmail.com>
2024-10-01 13:32:52 +0200

    feat: pre-defined spots for content in the world
    * allows spots to be defined within Blender and exported as GLTF
    * automatically and deterministic placement of pieces on spots
    * distributes content evenly across hot-spots

---
+++ components/world/project-box.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { MeshProps, ThreeEvent } from "@react-three/fiber";
+import { useRouter } from "next/navigation";
+import { useCallback, useRef, useState } from "react";
+import { Mesh } from "three";
+
+export function ProjectBox({ href, ...props }: { href: string } & MeshProps) {
+  const router = useRouter();
+  const ref = useRef<Mesh | null>(null);
+  const [hovered, setHovered] = useState(false);
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      castShadow
+      receiveShadow
+      onClick={useCallback(
+        (event: ThreeEvent<MouseEvent>) => {
+          if (href) {
+            event.stopPropagation();
+            router.push(href);
+          }
+        },
+        [href, router],
+      )}
+      onPointerOver={useCallback(
+        (event: ThreeEvent<PointerEvent>) => {
+          if (href) {
+            event.stopPropagation();
+            setHovered(true);
+          }
+        },
+        [href],
+      )}
+      onPointerOut={useCallback(
+        (event: ThreeEvent<PointerEvent>) => {
+          if (href) {
+            event.stopPropagation();
+            setHovered(false);
+          }
+        },
+        [href],
+      )}
+    >
+      <boxGeometry args={[0.4, 0.05, 0.6]} />
+      <meshPhysicalMaterial
+        color={href ? (hovered ? "yellow" : "khaki") : "lightgray"}
+      />
+    </mesh>
+  );
+}
