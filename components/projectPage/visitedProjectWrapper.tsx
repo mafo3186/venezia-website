1292163f1bfbba8be4cebdbcc5358ec3785d0e50
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-04 12:30:15 +0200

    73 hide unvisited hamburger menu items (#109)
    * #73 integrated manual Anagram-option in Menu
    
    * #73 added visitedProjectWrapper for usage in server component (projectPage)
    
    * #73 use visitedProjectWrapper  in projectPage
    
    * #73 update visited-status in menu immediately (not just after reload) and fix colors

---
+++ components/projectPage/visitedProjectWrapper.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useEffect } from 'react';
+
+interface VisitedProjectWrapperProps {
+  slug: string;
+}
+
+export default function VisitedProjectWrapper ({ slug }: VisitedProjectWrapperProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const visitedProjects = JSON.parse(localStorage.getItem('visitedProjects') || '[]');
+      if (!visitedProjects.includes(slug)) {
+        const updatedVisitedProjects = [...visitedProjects, slug];
+        localStorage.setItem('visitedProjects', JSON.stringify(updatedVisitedProjects));
+
+        // Dispatch event AFTER updating localStorage
+        window.dispatchEvent(new Event('visitedProjectsUpdated'));
+        console.log("visitedProjectsUpdated Event für " + slug);
+      }
+    }
+  }, [slug]);
+
+  return null;
+};
