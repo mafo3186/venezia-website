6e2215a39095c8a2a1deef758157eb98ff2b2a81
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-05 19:42:21 +0200

    #111 added author to menu (also as anagram)

--- components/menu.tsx
+++ components/menu.tsx
@@ -77,8 +77,8 @@ const Menu = ({ projects }: MenuProps) => {
     localStorage.removeItem('visitedProjects');
   };
 
-  const generateAnagram = (title: string) => {
-    return title.split('').sort(() => Math.random() - 0.5).join('');
+  const generateAnagram = (string: string) => {
+    if (string) return string.split('').sort(() => Math.random() - 0.5).join('');
   };
 
   //sucks, aber useRouter funktioniert nicht in dieser Komponente
@@ -119,6 +119,8 @@ const Menu = ({ projects }: MenuProps) => {
                           className={shouldShowAsVisited ? styles.visited : styles.unvisited}
                         >
                           {shouldShowAsVisited ? project.title : generateAnagram(project.title)}
+                          {" – "}
+                          {shouldShowAsVisited ? project.author : generateAnagram(project.author)}
                         </Link>
                       </li>
                     );
