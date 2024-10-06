87be756437760305439672fd318628713c918206
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-06 18:18:07 +0200

    #106 style of static Pages adapted to global style.

--- components/staticPages/fremde.tsx
+++ components/staticPages/fremde.tsx
@@ -11,10 +11,10 @@ export default function Fremde() {
           <HomeButton/>
           <BackButton/>
         </div>
-        <h1 className={styles.title}>Fremde überall</h1>
-        <h2 className={styles.title}>Biennale Venedig 2024</h2>
+        <h1>Fremde überall</h1>
+        <h2>Exkursion zur 60. Kunst-Biennale in Venedig 2024</h2>
         <br/>
-        <p className={styles.bold}>Exkursion</p>
+        <h3>Exkursion</h3>
         <p>Im Rahmen unserer Exkursion nach Venedig zur Biennale 2024, deren Thema „Fremde überall“ eine zentrale Rolle
           spielte, tauchten wir tief in die Stadt und ihre Atmosphäre ein. Auf der Insel Lido lebend, erkundeten wir
           nicht nur die kunstvollen Ausstellungen in Giardini und Arsenale, sondern auch die vielfältigen, im gesamten
@@ -23,11 +23,14 @@ export default function Fremde() {
           sich in den ausgestellten Werken auf eindrucksvolle Weise widerspiegelte. Diese Erfahrung eröffnete uns neue
           Perspektiven auf das Gefühl der Fremdheit und bereicherte unser Verständnis für das künstlerische
           Auseinandersetzen mit diesem bedeutenden und aktuellen Thema.</p>
-        <p className={styles.bold}>Webseite</p>
-        <p>Diese Webseite dient als Projektdokumentation. Zu den Projekten mit dem Thema &apos;Fremde überall&apos; kann
-          entweder mit dem Kompass navigiert werden, oder durch eine eigene Entdeckungsreise durch eine fremde und
-          vielleicht doch vertraute Welt.</p>
-        <IconKompass height={'50%'} width={'50%'}/>
+        <h3>Webseite</h3>
+        <p>Diese Webseite dient als Projektdokumentation. Alle Projekte beschäftigen sich mit dem Thema der Biennale &apos;Fremde überall&apos;. Die Erkundung der Projekte auf dieser Webseite verschreibt sich ebenfalls der Fremde.</p>
+        <h3>Navigation</h3>
+        <p>Zu den Projekten kann entweder mit dem Kompass navigiert werden, oder durch eine eigene Entdeckungsreise durch eine fremde und
+          vielleicht doch vertraute Welt, oder über eine Liste.</p>
+        <div className={styles["icon-container"]}>
+          <IconKompass height={'90%'} width={'90%'}/>
+        </div>
       </div>
     </div>
   );
