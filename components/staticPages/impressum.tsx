d01467988608732832ec5e030e372cbd7bd9cca4
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-13 18:31:23 +0200

    #125 new Datenschutz and Impressum (also improved text-styles)

--- components/staticPages/impressum.tsx
+++ components/staticPages/impressum.tsx
@@ -5,22 +5,32 @@ export default function Impressum() {
   return (
     <div className={styles.pageContainer}>
       <div className={styles.navigationButtons}>
-        <HomeButtonSwitcher/>
+        <HomeButtonSwitcher />
       </div>
       <div className={styles.textContainer}>
-
         <h1>Impressum</h1>
         <div className={styles.bold}>
-          <p>Hochschule Düsseldorf</p>
-          <p>University of Applied Sciences</p>
+          <p>
+            Hochschule Düsseldorf<br />
+            University of Applied Sciences
+          </p>
         </div>
-        <p>Münsterstraße 156, Gebäude 2</p>
-        <p>Tel.: +49 211 4351-0</p>
-        <p>Fax: +49 211 4351-19000</p>
-        <p>empfang@hs-duesseldorf.de</p>
-        <br/>
-        <p>Die Hochschule Düsseldorf ist eine Körperschaft des Öffentlichen Rechtes. Sie wird durch die Präsidentin,
-          Prof. Dr. Edeltraud Vomberg, gesetzlich vertreten.</p>
+        <p>
+          Münsterstraße 156, Gebäude 2<br />
+          40476 Düsseldorf<br />
+          Tel.: +49 211 4351-0<br />
+          Fax: +49 211 4351-19000
+        </p>
+        <h3>Verantwortlich für den Inhalt</h3>
+        <p>
+          Studentisches Projektteam unter der Leitung von Prof. Gabriele Schwab-Trapp<br />
+          Fachbereich Medien<br />
+          Hochschule Düsseldorf
+        </p>
+        <p>
+          Die Hochschule Düsseldorf ist eine Körperschaft des Öffentlichen Rechtes. Sie wird durch die Präsidentin,
+          Prof. Dr. Edeltraud Vomberg, gesetzlich vertreten.
+        </p>
       </div>
     </div>
   );
