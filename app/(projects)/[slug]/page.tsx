81a7fcf1ceb401b60e814a2f6e30da2b069a0b02
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2024-10-14 01:14:17 +0200

    feat: close project page when clicking outside

--- app/(projects)/[slug]/page.tsx
+++ app/(projects)/[slug]/page.tsx
@@ -1,9 +1,14 @@
 import ProjectPage, { Props } from "@/components/projectPage/projectPage";
+import Link from "next/link";
 export {
   generateMetadata,
   generateStaticParams,
 } from "@/components/projectPage/projectPage";
 
 export default async function ProjectPage3D({ params }: Props) {
-  return <ProjectPage params={params} />;
+  return (
+    <Link href="/">
+      <ProjectPage params={params} />;
+    </Link>
+  )
 }
