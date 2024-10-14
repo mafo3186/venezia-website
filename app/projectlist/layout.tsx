b155c94015e95ae79f822e683a76e9e0a8037b9d
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-14 04:22:18 +0200

    130 new responsive styling of structured view (#143)
    * #130 refactoring projectList (names and components)
    
    * #130 first alternative listView with kind of waves
    
    * #130 removed h1 and reorganized text/points
    
    * #130 changed text-style and background-color
    
    * #130 added map as background image (doesn't work with waves)
    
    * #130 map without waves
    
    * #130 adding underWater scenarios as background images for structured view
    
    * #130 adding under water scenario
    
    * #130 dark watery font and point styles
    
    * #130 added "subtitle"
    
    * #130 added "subtitle"
    
    * #130 added second option as "door" style for projects (not active)
    
    * #130 added background image to layout (not removed from list-component yet)
    
    * #130 fix missing style-name textContainer
    
    * #130 adapted colorscheme of menu
    
    * #130 adapted colorscheme of menu again
    
    * #130 remove double background-image and fix style-differences between list and 3D projectpages
    
    * #130 remove unused imports

--- app/projectlist/layout.tsx
+++ app/projectlist/layout.tsx
@@ -2,11 +2,19 @@
 import '@/app/global.css';
 
 import { PropsWithChildren } from "react";
+import listStyles from "@/components/list/projectList.module.css"; 
+
 
 export default function ProjectListLayout({ children }: PropsWithChildren<{}>) {
   return (
     <main>
-      {children}
+      <div className={listStyles.pageContainer}>
+        <div className={listStyles.backgroundImage}>
+          <div className={listStyles.title}>Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024</div>
+        </div>
+        <div className={listStyles.backgroundImage}></div>
+        {children}
+      </div>
     </main>
-  );
+);
 }
---
+++ components/list/projectList.tsx
@@ -0,0 +1,38 @@
+// components/ListProjects.tsx
+
+/* eslint-disable @next/next/no-img-element */
+
+import { sanityFetch } from "@/sanity/lib/fetch";
+import type { ProjectsListQueryResult } from "@/sanity.types";
+
+import listStyles from "@/components/list/projectList.module.css";
+import { projectsListQuery } from "@/sanity/lib/queries";
+import Link from "next/link";
+
+export default async function ProjectList() {
+  const projects = await sanityFetch<ProjectsListQueryResult>({ query: projectsListQuery, params: { orderBy: "title" } });
+
+  const projectPositions = projects.map((project, index) => ({
+    title: project.title,
+    slug: project.slug,
+    author: project.author || "unbekannt",
+    description: project.description,
+  }));
+
+  return (
+    <>
+      <div className={listStyles.projectContainer}>
+        {projectPositions.map((project) => (
+          <div key={project.slug} className={listStyles.projectPointContainer}>
+            <Link href={`/projectlist/${project.slug}`} className={listStyles.projectPointContainer}>
+              <div className={listStyles.projectPoint}></div>
+              <div className={listStyles.projectTitle}>{project.title}</div>
+              <div className={listStyles.projectAuthor}>{project.author}</div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className={listStyles.wave}></div>
+    </>
+  );
+}
