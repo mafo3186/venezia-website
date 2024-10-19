6839edcbf4137e35d3b6813d72d64f35207b47d8
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-19 22:38:29 +0200

    #123 aria-label for hidden "title", Link and padding

--- app/projectlist/layout.tsx
+++ app/projectlist/layout.tsx
@@ -2,18 +2,25 @@
 import '@/app/global.css';
 
 import { PropsWithChildren } from "react";
-import listStyles from "@/components/list/projectList.module.css"; 
+import listStyles from "@/components/list/projectList.module.css";
+import Link from "next/link"; 
 
 
 export default function ProjectListLayout({ children }: PropsWithChildren<{}>) {
   return (
     <main>
       <div className={listStyles.pageContainer}>
-        <div className={listStyles.backgroundImage}>
-          <div className={listStyles.title}>Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024</div>
-        </div>
+        <div className={listStyles.backgroundImage}></div>
         {children}
+        <div className={listStyles.title}>
+          <Link 
+            href={"/projectlist/fremde"}
+            aria-label={"Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024"}
+          >
+            Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024
+          </Link>
+        </div>
       </div>
     </main>
-);
+  );
 }
