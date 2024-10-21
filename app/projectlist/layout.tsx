bb50aa34331edd682908a31e34c73bcabd9879e5
mafo3186 <73349129+mafo3186@users.noreply.github.com>
2024-10-21 18:49:57 +0200

    #161 remove Link of Subtitle to remove overlay, but add aria-label for screen-reader

--- app/projectlist/layout.tsx
+++ app/projectlist/layout.tsx
@@ -10,16 +10,15 @@ export default function ProjectListLayout({ children }: PropsWithChildren<{}>) {
   return (
     <main>
       <div className={listStyles.pageContainer}>
-        <div className={listStyles.backgroundImage}></div>
-        {children}
-        <div className={listStyles.title}>
-          <Link 
-            href={"/projectlist/fremde"}
-            aria-label={"Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024"}
+        <div className={listStyles.backgroundImage}>
+          <div 
+            className={listStyles.title}
+            aria-label="Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024"
           >
             Fremde überall - Exkursion zur Kunst-Biennale Venedig 2024
-          </Link>
+          </div>
         </div>
+        {children}
       </div>
     </main>
   );
