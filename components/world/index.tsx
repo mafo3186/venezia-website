43c4f83920e719b93f8294dc97b716b34c63e0e4
Luca Selinski <lucaselinski@gmail.com>
2024-10-24 02:50:42 +0200

    feat: visually appealing content pieces

--- components/world/index.tsx
+++ components/world/index.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { PropsWithChildren, useMemo } from "react";
 import { SceneCanvas } from "./scene";
 import styles from "./world.module.css";
-import { HotspotsWithProjects, Spot } from "@/components/types";
+import { HotspotsWithProjects } from "@/components/types";
 import { useHotspot, useVisited } from "@/components/contexts";
 
 function Content({
@@ -20,11 +20,9 @@ function Content({
 
 export function CanvasContainer({
   projects,
-  emptySpots,
   children,
 }: PropsWithChildren<{
   projects: HotspotsWithProjects;
-  emptySpots: Spot[];
 }>) {
   const pathname = usePathname();
   const { view, onViewReached } = useHotspot();
@@ -52,7 +50,6 @@ export function CanvasContainer({
       <div className={styles.home}>
         <SceneCanvas
           projects={projects}
-          emptySpots={emptySpots}
           inBackground={onChildPage}
           view={view}
           onViewReached={onViewReached}
